66ce5a4a71b3b59ddca8d9fbc3d02f396736055e
NelsonGilberto1 <168493132+NelsonGilberto1@users.noreply.github.com>
2026-05-08 20:34:31 +0000

    Ex02

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,22 +6,45 @@ import './App.css'
 
 //Css:
 import './Ex01Css/Estilos.css'
+import './Ex02Css/Listas.css'
 
 //Jsx:
 import NomeAluno from './Ex01Css/NomeAluno'
 import Curso from './Ex01Css/Curso'
 import Botaover from './Ex01Css/BotaoVer'
+import Listas from './Ex02Css/Listas';
 
 function App() {
-  
+
 
   return (
     <div>
       
-
+      <h1>Exercicio 3 - Menu Horizontal</h1>
+      
 
       {/**
        *
+       * 
+      Ex 02:
+
+      const Produtos = [
+        {id: 1, nome: 'D-10', preco: 'R$: 40.000', className: 'id1'},
+        {id: 2, nome: 'D-20', preco: 'R$: 60.000', className: 'id2'},
+        {id: 3, nome: 'S-10', preco: 'R$: 80.000', className: 'id3'}
+      ];
+
+      <h2>Lista de Produtos:</h2>
+
+      <ul>
+         {Produtos.map((produto) =>(
+          <li key={produto.id} className={produto.className}>
+            {produto.nome} - {produto.preco}
+          </li>
+        ))}
+      </ul>
+
+      Ex 01:
       <div className='borda'>
         <h2 className='texto'>Card Simpes:</h2> 
         <NomeAluno className='texto'/>
---
+++ my-app/src/Ex02Css/Listas.jsx
@@ -0,0 +1,8 @@
+const Listas = [
+    {id: 1, nome: 'D-10', preco: 'R$: 40.000'},
+    {id: 2, nome: 'D-20', preco: 'R$: 60.000'},
+    {id: 3, nome: 'S-10', preco: 'R$: 80.000'}
+  ];
+
+
+export default Listas
